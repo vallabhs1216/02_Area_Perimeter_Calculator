60703092201fa9fe0f658ee147b05ab3c7ce6afe
vallabhs1216 <vallabhs1216@masseyhigh.school.nz>
2022-02-28 09:26:27 +1300

    Create 01_looping_basic.py

---
+++ 01_looping_basic.py
@@ -0,0 +1,11 @@
+valid = False
+while not valid:
+
+    response = float(input("Enter a number: "))
+
+    if response > 0:
+        valid = True
+    
+    else:
+        print("Please enter a number that is more then zero ")
+        print()
